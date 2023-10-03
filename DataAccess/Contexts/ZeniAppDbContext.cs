9b9dc5cfe95ce28333b62b25dfa38a8244fbb3e0
itishadi <113441103+itishadi@users.noreply.github.com>
2023-10-03 09:15:56 +0200

    Add project files.

---
+++ DataAccess/Contexts/ZeniAppDbContext.cs
@@ -0,0 +1,34 @@
+﻿using DataAccess.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Contexts
+{
+    public class ZeniAppDbContext : DbContext
+    {
+        public ZeniAppDbContext()
+        {
+
+        }
+
+
+        public ZeniAppDbContext (DbContextOptions<ZeniAppDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+            try
+            {
+                Database.Migrate();
+            }
+            catch { }
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite();
+        }
+        public DbSet<ZeniAppSettings> Settings { get; set; }
+    }
+}
---
+++ DataAccess/Models/Entities/ZeniAppSettings.cs
@@ -0,0 +1,10 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace DataAccess.Models.Entities;
+
+public class ZeniAppSettings
+{
+    [Key]
+    public string ConnectionString { get; set; } = null!;
+}
